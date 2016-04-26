472a0ccb6b5c2133cd59f34880088a582a98259f
Remi Daigle <remi.daigle@dal.ca>
2016-04-26 16:02:00 -0700

    data for Jotunheimen, Breheimen, Utladalen

--- getphotos.R
+++ getphotos.R
@@ -1,14 +1,25 @@
 #### load R packages ####
 # devtools::install_github("remi-daigle/flickRgeotag")
 library(flickRgeotag)
+library(rgdal)
 
-api_key <- ''
+api_key <- '' # if you want to run this again, get your own API key from flickr
+
+### load Jotunheimen (and neighbours) ###
+parks <- readOGR("shapefiles","jotunheimen")
+plot(parks)
+parks$NAME
 
 #### set search parameters ####
-bbox <- '3,57,31,72'
+bbox <- paste0(as.character(bbox(parks)), collapse = ",")
+
 
 #### download photo metadata ####
 sub_bbox_Norway <- splitbbox(api_key, bbox)
 writeLines(sub_bbox_Norway,"sub_bbox_Norway")
 sub_bbox_Norway <- readLines("sub_bbox_Norway")
 photos <- flickr.meta.dl(api_key, sub_bbox_Norway)
+write.csv(photos,'photos.csv')
+
+#### exploratory plot ####
+qflickr.plot(photos)
